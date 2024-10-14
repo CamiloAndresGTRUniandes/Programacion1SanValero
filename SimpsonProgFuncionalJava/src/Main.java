30c76151ac39e0e1a7588097a2c9347fcc949c1e
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-10-14 13:57:21 +0200

    Movimientos inical

--- SimpsonProgFuncionalJava/src/Main.java
+++ SimpsonProgFuncionalJava/src/Main.java
@@ -1,14 +1,37 @@
 import java.util.Random;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static final Random aleatorio = new Random();
+    private static final Scanner scanner = new Scanner(System.in);
     private static final int MAX_FILA_TABLERO = 10;
     private static final int MAX_COLUMNA_TABLERO = 10;
-
+    private static int MAX_VIDAS = 3;
     public static char [][] tablero;
-
+    private static int[] posicionBart = new int[2];
     public static void main(String[] args) {
+        //InicializarTablero
+        inicializarTablero();
+        while (true){
+            if (MAX_VIDAS == 0) {
+                System.out.println("Game Over!");
+                break;
+            }
+            System.out.println("Use W (arriba), A (izquierda), S (abajo), D (derecha) para mover a Bart. Digite 'S' para terminar.");
+            char movimiento = scanner.nextLine().toUpperCase().charAt(0);
+            if (movimiento == 'S') {
+                break;
+            }
+
+            moverBart(movimiento);
+            imprimirTablero();
+        }
+
+
+    }
+    private static void inicializarTablero(){
         //Inicializar la matriz del tablero
         tablero = new char[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
 
@@ -18,45 +41,20 @@ public class Main {
                 tablero[i][j] = 'L';
             }
         }
-        //Imprimir el tablero
-       imprimirTablero();
 
-        //Asignar a Bart
-        Random aleatorio = new Random();
-        int filaAleatorio = aleatorio.nextInt(MAX_FILA_TABLERO);
-        int columnaAleatorio = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
-        tablero[filaAleatorio][columnaAleatorio] = 'B';
-
-        imprimirTablero();
+        //Asignar a Bart a posición inicial
+        asignarBart();
 
         //Repartir 10 Homer en el tablero
-        for (int i = 0; i < 10; i++){
-            int filaAleatorioHomer = aleatorio.nextInt(MAX_FILA_TABLERO);
-            int columnaAleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
-            char valor = tablero[filaAleatorioHomer][columnaAleatorioHomer];
-            if (valor == 'L')
-            {
-                tablero[filaAleatorioHomer][columnaAleatorioHomer] = 'H';
-            }
-        }
-        imprimirTablero();
+        generarHomer();
 
         //Imprimir Muro
-        for (int i = 0; i < 10; i++){
-            int filaAleatorioMuro = aleatorio.nextInt(MAX_FILA_TABLERO);
-            int columnaAleatorioMuro = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
-            char valor = tablero[filaAleatorioMuro][columnaAleatorioMuro];
-            if (valor == 'L')
-            {
-                tablero[filaAleatorioMuro][columnaAleatorioMuro] = 'M';
-            }
-        }
-        imprimirTablero();
+        generarMuro();
+
 
         //Asignar final
         tablero[MAX_FILA_TABLERO -1][MAX_COLUMNA_TABLERO -1] = 'F';
         imprimirTablero();
-
     }
     private static void imprimirTablero(){
         for (int i = 0; i < MAX_FILA_TABLERO; i++){
@@ -69,5 +67,64 @@ public class Main {
         System.out.println(" ");
         System.out.println(" ");
         System.out.println(" ");
+    }
+    private static void generarHomer(){
+        for (int i = 0; i < 10; i++){
+            int filaAleatorioHomer = aleatorio.nextInt(MAX_FILA_TABLERO);
+            int columnaAleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
+            char valor = tablero[filaAleatorioHomer][columnaAleatorioHomer];
+            if (valor == 'L')
+            {
+                tablero[filaAleatorioHomer][columnaAleatorioHomer] = 'H';
+            }
+        }
+    }
+
+    private static void asignarBart(){
+        int filaAleatorio = aleatorio.nextInt(MAX_FILA_TABLERO);
+        int columnaAleatorio = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
+        tablero[filaAleatorio][columnaAleatorio] = 'B';
+        posicionBart[0] = filaAleatorio;
+        posicionBart[1] = columnaAleatorio;
+    }
+    private static void generarMuro(){
+        for (int i = 0; i < 10; i++){
+            int filaAleatorioMuro = aleatorio.nextInt(MAX_FILA_TABLERO);
+            int columnaAleatorioMuro = aleatorio.nextInt(MAX_COLUMNA_TABLERO);
+            char valor = tablero[filaAleatorioMuro][columnaAleatorioMuro];
+            if (valor == 'L')
+            {
+                tablero[filaAleatorioMuro][columnaAleatorioMuro] = 'M';
+            }
+        }
+    }
+
+    private static void moverBart(char movimiento){
+        int fila = posicionBart[0];
+        int columna = posicionBart[1];
+        tablero[fila][columna] = 'L';
+
+        switch (movimiento){
+            case 'D':
+                if (columna < MAX_COLUMNA_TABLERO - 1) {
+                    if (tablero[fila][columna+1] == 'H') {
+                        System.out.println("Te has chocado con Homer");
+                        MAX_VIDAS --;
+                        break;
+                    } else if (tablero[fila][columna+1] == 'M') {
+                        System.out.println("Te has chocado con un muro, movimiento inválido");
+                        break;
+                    }
+                    columna++;
+                }else{
+                    System.out.println("Fuera del limite");
+                }
+            default:
+                System.out.println("Movimiento no válido. Usa W, A, S, D.");
+                break;
+        }
+        posicionBart[0] = fila;
+        posicionBart[1] = columna;
+        tablero[fila][columna] = 'B';
     }
 }
